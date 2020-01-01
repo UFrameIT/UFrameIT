93c0477513d7402ee7ae25d22d0b99ebabc71f9b
BenniHome <benjamin.boesl@studium.fau.de>
2020-01-01 18:16:31 +0100

    automagic- scrollgeneration begun

--- Assets/InventoryStuff/DisplayScrolls.cs
+++ Assets/InventoryStuff/DisplayScrolls.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class DisplayScrolls : MonoBehaviour
 {
-    //public Inventory inventory;
+    public List<Scroll> scrolls = new List<Scroll>();
+
 
 
     public int x_Start;
@@ -13,11 +15,7 @@ public class DisplayScrolls : MonoBehaviour
     public int X_Pacece_Between_Items;
     public int y_Pacece_Between_Items;
     public int number_of_Column;
-    // Start is called before the first frame update
-    void Start()
-    {
-        //CreateDisplay();
-    }
+   
 
     // Update is called once per frame
     void Update()
@@ -42,5 +40,17 @@ public class DisplayScrolls : MonoBehaviour
     public Vector3 GetPosition(int i)
     {
         return new Vector3(x_Start+ (X_Pacece_Between_Items * (i % number_of_Column)), y_Start + (-y_Pacece_Between_Items * (i / number_of_Column)), 0f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string path = "Mock-Scrolls.json";
+        string jsonString = File.ReadAllText(path);
+        jsonString = jsonString.Replace(System.Environment.NewLine, "");
+        jsonString = jsonString.Replace("\t", "");
+        Debug.Log(jsonString);
+        Scroll[] scrollsRead = JsonUtility.FromJson<Scroll[]>(jsonString);
+        //this.scrolls = scrollsRead;
     }
 }
---
+++ Assets/InventoryStuff/Scroll.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+[Serializable]
+public class Declaration
+{
+    public string name;
+    public string type;
+    public string identifier;
+}
+
+
+[Serializable]
+public class Scroll
+{
+    public string problemTheory;
+    public string solutionTheory;
+    public string label;
+    public string description;
+    public Declaration[] declarations;
+    //public string output;
+
+    public static Scroll generateFromJson(string json) {
+       return JsonUtility.FromJson<Scroll>(json);
+    }
+
+    public string toJson() {
+        return JsonUtility.ToJson(this);
+    } 
+}
+
