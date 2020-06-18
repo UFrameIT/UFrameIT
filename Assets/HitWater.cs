461850179a9426f604d8e8a65229f17cbd46ee68
unknown <john.schihada@hotmail.com>
2020-06-18 17:09:42 +0200

    Added new RiverScene and therefore: Added a terrain with a river, Added Water-Cube, Adjusted forest-elements, Added script when FPSController hits water, disabled jumping with adjusting controller-jumping-speed

---
+++ Assets/HitWater.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitWater : MonoBehaviour
+{
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Water")) {
+            transform.position = new Vector3(-2.68f, 1f, 1.89f);
+        }
+    }
+}
--- Assets/Standard Assets/Utility/ForcedReset.cs
+++ Assets/Standard Assets/Utility/ForcedReset.cs
@@ -1,9 +1,10 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityStandardAssets.CrossPlatformInput;
 
-[RequireComponent(typeof (GUITexture))]
+[RequireComponent(typeof (Texture))]
 public class ForcedReset : MonoBehaviour
 {
     private void Update()
